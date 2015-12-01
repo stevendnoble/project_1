13fb1540a2a4d68bce97c6946ad84e942e1e8ba1
alanlee35 <alanblee12@gmail.com>
2015-12-01 12:00:20 -0800

    working on mapbox

--- public/main.js
+++ public/main.js
@@ -10,6 +10,24 @@ $(function () {
 	var source = $('#event-template').html(),
 			template = Handlebars.compile(source);
 
+
+	//mapbox
+L.mapbox.accessToken = 'pk.eyJ1IjoiYWxhbmJsZWUzNSIsImEiOiJjaWhucHNlMzAwN28zdTJqN3h3cTF2aGxlIn0.xTG9793sG8BlSn54rmHSUA';
+var geocoder = L.mapbox.geocoder('mapbox.places'),
+    map = L.mapbox.map('map', 'mapbox.streets');
+
+geocoder.query('San Francisco, CA', showMap);
+
+function showMap(err, data) {
+    // The geocoder can return an area, like a city, or a
+    // point, like an address. Here we handle both cases,
+    // by fitting the map bounds to an area or zooming to a point.
+    if (data.lbounds) {
+        map.fitBounds(data.lbounds);
+    } else if (data.latlng) {
+        map.setView([data.latlng[0], data.latlng[1]], 13);
+    }
+}
 	//append events
 	var appendEvent = function () {
 		$eventList.empty();
@@ -29,7 +47,7 @@ $submitSearch.on('submit', function (event) {
 			};
 	//api call with searchParams
 	$.get('/api/events', searchParam, function (data) {
-		console.log(data.events.event);
+		console.log(data);
 		eventCollection = data.events.event;
 		appendEvent();
 	});
